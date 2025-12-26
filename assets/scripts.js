7e527a8a8fb2781c21734eff102842fb626d424b
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2025-12-26 21:22:17 +1300

    improve responsiveness

--- assets/scripts.js
+++ assets/scripts.js
@@ -54,27 +54,43 @@ document.addEventListener("DOMContentLoaded", () => {
     const navicon = document.querySelector(".nf-fa-navicon");
     const backicon = document.querySelector(".nf-fa-angle_left");
     const navbtn = document.getElementById("navbtn");
+    const fullnav = document.getElementById("fullnav");
+    const fullnavbtn = document.getElementById("fullnavbtn");
 
     // initial state
     head1.style.display = "block";
     head2.style.display = "none";
     navicon.style.display = "block";
     backicon.style.display = "none";
+    fullnavbtn.style.display = "none";
 
     navbtn.addEventListener("click", () => {
         const isOpen = head2.style.display === "flex";
-        if (!isOpen) {
-            // open menu
-            head1.style.display = "none";
-            head2.style.display = "flex";
-            navicon.style.display = "none";
-            backicon.style.display = "block";
+        const isResponsive = window.innerWidth < 600;
+        if (!isResponsive) {
+            if (!isOpen) {
+                // open menu
+                head1.style.display = "none";
+                head2.style.display = "flex";
+                navicon.style.display = "none";
+                backicon.style.display = "block";
+            } else {
+                // close menu
+                head1.style.display = "block";
+                head2.style.display = "none";
+                navicon.style.display = "block";
+                backicon.style.display = "none";
+            }
         } else {
-            // close menu
-            head1.style.display = "block";
-            head2.style.display = "none";
-            navicon.style.display = "block";
-            backicon.style.display = "none";
+            if (!isOpen) {
+                // open full nav
+                fullnav.style.display = "flex";
+                fullnavbtn.style.display = "flex";
+            }
         }
+    });
+    fullnavbtn.addEventListener("click", () => {
+        fullnav.style.display = "none";
+        fullnavbtn.style.display = "none";
     });
 });
