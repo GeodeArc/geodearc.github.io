69ebc9386055425b75aca606d55ca25c368dfe33
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2025-12-26 20:29:29 +1300

    redesign start

--- assets/scripts.js
+++ assets/scripts.js
@@ -36,15 +36,45 @@ document.addEventListener("DOMContentLoaded", () => { // theme switching
 });
 
 // CLIPBOARD COPY (copyClipboard)
-async function copyClip(element) {
-    element = element || document.querySelector(".copy");
+function copyClip() {
+    const text = document.querySelector('.copy').innerText;
+    navigator.clipboard.writeText(text);
 
-    const text = element.innerText;
-    await navigator.clipboard.writeText(text);
-
-    element.innerText = "(copied to clipboard :D)";
+    const icon = document.getElementById('copyicon');
+    icon.className = 'nf-fa-check';
 
     setTimeout(() => {
-        element.innerText = text;
-    }, 2000);
+        icon.className = 'nf-fa-copy';
+    }, 1000);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const head1 = document.querySelector(".head1");
+    const head2 = document.querySelector(".head2");
+    const navicon = document.querySelector(".nf-fa-navicon");
+    const backicon = document.querySelector(".nf-fa-angle_left");
+    const navbtn = document.getElementById("navbtn");
+
+    // initial state
+    head1.style.display = "block";
+    head2.style.display = "none";
+    navicon.style.display = "block";
+    backicon.style.display = "none";
+
+    navbtn.addEventListener("click", () => {
+        const isOpen = head2.style.display === "flex";
+        if (!isOpen) {
+            // open menu
+            head1.style.display = "none";
+            head2.style.display = "flex";
+            navicon.style.display = "none";
+            backicon.style.display = "block";
+        } else {
+            // close menu
+            head1.style.display = "block";
+            head2.style.display = "none";
+            navicon.style.display = "block";
+            backicon.style.display = "none";
+        }
+    });
+});
